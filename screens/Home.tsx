22cd4e06e0da761c643dd86844ec5490e2943b4d
chirag-unision <sharmacj123@gmail.com>
2023-04-23 19:12:53 +0530

    maps implemented

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,38 +1,50 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, Platform } from 'react-native'
 import Geolocation from '@react-native-community/geolocation'
+import  MapmyIndiaGL  from  'mapmyindia-map-react-native-beta';
+
 type Props = {}
 
 export default function Home({}: Props) {
+    const [lat, setLat] = useState(0);
+    const [lan, setLan] = useState(0);
 
-    const getCurrentLocation = () => {
-        return new Promise((resolve, reject) => {
-          if (Platform.OS === 'ios') {
-            Geolocation.requestAuthorization();
-          }
-          Geolocation.getCurrentPosition(
-            position => {
-              const {latitude, longitude} = position.coords;
-              resolve({latitude, longitude});
-            },
-            error => reject(error),
-            {
-              enableHighAccuracy: true,
-              timeout: 20000,
-              maximumAge: 1000,
-            },
-          );
-        });
-      };
+    const atlasClientId='33OkryzDZsLwiwCWNUPQFHhJ1yPnKA_EjnBuB3_WtRKpgMjzqmEXyJPUA2Ji9LqJbfIWo3c9tR8c5mRO_BMkPA==';
+    const atlasClientSecret='lrFxI-iSEg-jBKzZuvf4DRwt2xGn_hxvItc_NIeqU7THRRGDbTfYK1t20PZU6hZZBLA_II0I2i7AglFAse7EkIqMUSVbLeEO';
+    const mapSDKKey='bfda91035e35188fd54897f55c730daa';
+    const restAPIKey='bfda91035e35188fd54897f55c730daa';
+    
+
+    MapmyIndiaGL.setMapSDKKey(mapSDKKey);//place your mapsdkKey
+    MapmyIndiaGL.setRestAPIKey(restAPIKey);//your restApiKey
+    MapmyIndiaGL.setAtlasClientId(atlasClientId);//your atlasClientId key
+    MapmyIndiaGL.setAtlasClientSecret(atlasClientSecret); //your atlasClientSecret key
+
 
       useEffect(()=>{
-        console.log(getCurrentLocation());
+        Geolocation.getCurrentPosition((data)=>{
+            setLat(data.coords.latitude);
+            setLan(data.coords.longitude);
+            console.log(data.coords)
+        })
       },[])
       
   return (
     <View style={styles.container}>
         <View style={styles.top}></View>
-        <View style={styles.bottom}></View>
+        <View style={styles.bottom}>
+        <View style={{flex:1}}>
+            <MapmyIndiaGL.MapView style={{flex:1}} >
+            <MapmyIndiaGL.Camera
+                    ref={c  => (this.camera = c)}
+                    zoomLevel={12}
+                    minZoomLevel={4}
+                    maxZoomLevel={22}
+                    centerCoordinate={[77,28]}
+                    />
+            </MapmyIndiaGL.MapView>
+        </View>
+        </View>
     </View>
   )
 }
